a85b0d460ec76763b0ffb9730a43614294a6294c
STING_OP <nitinkumargreat73@gmail.com>
2022-05-10 00:32:16 +0530

    Result on the basis of selected value

---
+++ action.php
@@ -0,0 +1,17 @@
+<?php
+    if(isset($_POST['submit'])){
+    if(!empty($_POST['school']) && !empty($_POST['course']) && !empty($_POST['branch']) && !empty($_POST['semester'])) {
+        $school = $_POST['school'];
+        $course = $_POST['course'];
+        $branch = $_POST['branch'];
+        $semester = $_POST['semester'];
+        if($school=="USICT" && $course=="btech" && $branch=="CSE" && $semester=="4"){
+            echo "<script>window.location.href='AttPanel.html';</script>";
+        }
+        else{
+            echo"Please select USICT, BTECH, CSE, 4 sem to continue";
+            echo "<br><br><br>"."<a href='select.html'> Back to previous Page</a>";
+        }
+    } 
+}
+?>
