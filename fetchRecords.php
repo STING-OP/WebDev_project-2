b0926440a76387ffddcb89f0365fee8cde47bf29
STING_OP <nitinkumargreat73@gmail.com>
2022-07-31 12:52:41 +0530

    Api to fetch data using date

---
+++ fetchRecords.php
@@ -0,0 +1,15 @@
+<?php
+header('Content-Type:application/json');
+header('Access-Control-Allow-Origin:*');
+$data= json_decode(file_get_contents("php://input"), true);
+$date_val= $data['dateVal'];
+ $con=   mysqli_connect('localhost', 'root', '', 'ggsipuattendancedb') or  die("Connection failed: " . mysqli_connect_error());
+ $sql= "SELECT * FROM radiodata WHERE entryDate='{$date_val}'";
+ $res = mysqli_query($con, $sql) or die("Sql query failed");
+ if(mysqli_num_rows($res)>0){
+    $output= mysqli_fetch_all($res,MYSQLI_ASSOC);
+    echo json_encode($output);
+  }else{
+  echo json_encode(array('message'=>'No Record Found.','status'=>false));
+  }   
+?>
